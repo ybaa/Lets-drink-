9f7e8c99135cc9d4fa9781614e45f021973c91f6
Martyna Łagożna <m.lagozna@live.com>
2017-01-30 22:49:41 +0100

    read and write data from/to .json file; button works; temporary I don't care about type of beverage because in the future i'm gonna change it to List

---
+++ Lets drink!/Day.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lets_drink_ {
+    class Day {
+        public  string date { get; set; }
+        public  double goal { get; set; }
+        public  double currentDrunk { get; set; }
+        public  string typeOfBeverage { get; set; }
+
+        // public List<List<string>> typeOfBeverage;     //to do later
+    }
+}
--- Lets drink!/MainPage.xaml.cs
+++ Lets drink!/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,10 +25,125 @@ namespace Lets_drink_
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Day day = new Day();
+        
         public MainPage()
         {
             this.InitializeComponent();
+            setInitialValues();
+
+        }
+
+        private void setInitialValues() {
+            try {
+                typeOfBeverageComboBox.Items.Add("water");
+                typeOfBeverageComboBox.Items.Add("tea");
+                typeOfBeverageComboBox.Items.Add("juice");
+                typeOfBeverageComboBox.Items.Add("coffee");
+
+
+                string jsonString = File.ReadAllText("Assets/day.json");                
+                day = JsonConvert.DeserializeObject<Day>(jsonString);
+                if (checkIfItIsStillTheSameDay() == true) {
+                    currentDrunkTextBlock.Text = day.currentDrunk.ToString();
+                }
+                else {
+                    currentDrunkTextBlock.Text = "0";
+                }
+                goalTextBlock.Text = day.goal.ToString();
+
+            }
+            catch {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void firststButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                addToCurrentAfterClickButton(1);
+            }
+            catch {
+                throw new NotImplementedException();
+            }
             
+
+        }
+
+        private bool checkIfItIsStillTheSameDay() {
+            string date = DateTime.Now.ToString("dd-MM-yyyy");
+            if (date == day.date)
+                return true;
+            else
+                return false;
+        }
+
+        private void secondButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                addToCurrentAfterClickButton(2);
+            }
+            catch {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void thirdButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                addToCurrentAfterClickButton(3);
+            }
+            catch {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void fouthButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                addToCurrentAfterClickButton(4);
+            }
+            catch {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void addToCurrentAfterClickButton(int button) {
+            string choosedButton = "L";
+            switch (button) {
+                case 1:
+                    choosedButton = firststButton.Content.ToString();
+                    break;
+                case 2:
+                    choosedButton = secondButton.Content.ToString();
+                    break;
+                case 3:
+                    choosedButton = thirdButton.Content.ToString();
+                    break;
+                case 4:
+                    choosedButton = fouthButton.Content.ToString();
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (checkIfItIsStillTheSameDay() == true) {
+                if (typeOfBeverageComboBox.SelectedItem != null) {
+                    choosedButton = choosedButton.Replace("L", string.Empty);
+                    //test.Text = t.ToString();
+                    double currentTextFromButton = double.Parse(choosedButton);
+
+                    double currentValue = Math.Round(day.currentDrunk + currentTextFromButton, 2);
+                    currentDrunkTextBlock.Text = currentValue.ToString();
+                    day.currentDrunk = Math.Round(double.Parse(currentDrunkTextBlock.Text), 2);
+
+                    day.typeOfBeverage = typeOfBeverageComboBox.SelectedItem.ToString();
+
+                    string data = JsonConvert.SerializeObject(day);
+                    File.WriteAllText("Assets/day.json", data);
+                }
+                else {
+                }
+            }
+            else {
+            }
         }
     }
 }
